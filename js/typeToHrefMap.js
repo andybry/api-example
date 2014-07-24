551066f3d48aaf2a262d89bc98ff06aa28373a8d
Andy Bryant <ar.bryant@btinternet.com>
2014-07-24 02:18:57 +0100

    Map of type to API url

---
+++ js/typeToHrefMap.js
@@ -0,0 +1,9 @@
+/**
+ * Data map giving the category type to href
+ */
+var typeToHrefMap = {
+  news: 'http://api.mirror.co.uk/news/81894',
+  sport: 'http://api.mirror.co.uk/sport/81894',
+  celebrity: 'http://api.mirror.co.uk/3am/81894',
+  tv: 'http://api.mirror.co.uk/tv/81894'
+};
