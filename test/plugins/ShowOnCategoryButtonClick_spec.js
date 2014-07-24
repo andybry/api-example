70b7572d44a2c31a1aa0b4d97acfb058ff1efb69
Andy Bryant <ar.bryant@btinternet.com>
2014-07-24 01:20:25 +0100

    Tidy up spec for ShowOnCategoryButtonClick

--- test/plugins/ShowOnCategoryButtonClick_spec.js
+++ test/plugins/ShowOnCategoryButtonClick_spec.js
@@ -1,12 +1,13 @@
 describe('ShowOnCategoryButtonClick', function() {
 
-  it('Hides the element when a categoryButtonClick event is fired', function() {
+  it('Shows the element when a categoryButtonClick event is fired', function() {
     $('body').append(
       '<div id="sandbox" class="is-hidden"></div>'
     );
     $('#sandbox').ShowOnCategoryButtonClick();
     eventHub.fire('categoryButtonClick', 'celebrity');
     expect($('#sandbox').hasClass('is-hidden')).toBe(false);
+    $('#sandbox').remove();
   });
   
 });
