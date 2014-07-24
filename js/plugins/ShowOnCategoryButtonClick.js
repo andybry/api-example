d88921646038825ee579f60f1b19c4183db10b1c
Andy Bryant <ar.bryant@btinternet.com>
2014-07-24 01:12:18 +0100

    Add jsdoc for ShowOnCategoryButtonClick

--- js/plugins/ShowOnCategoryButtonClick.js
+++ js/plugins/ShowOnCategoryButtonClick.js
@@ -6,10 +6,18 @@
  */
 plugins.ShowOnCategoryButtonClick = Base.extend(AbstractPlugin, {
 
+  /**
+   * register the event handler
+   *
+   * @private
+   */
   _setup: function() {
     eventHub.register('categoryButtonClick', this);
   },
 
+  /**
+   * @param {string} type
+   */
   handleEvent: function(type) {
     this._$node.removeClass('is-hidden');
   }
